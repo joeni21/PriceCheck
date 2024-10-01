64754e51ca0b5112507604bbe1edf568cc90d6b7
Justin Oeni <joeni@andrew.cmu.edu>
2024-10-01 15:00:59 -0400

    Done

--- PriceCheckFB/DataRepositories/LocationRepository.swift
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -6,26 +6,33 @@
 
 import Combine
 // import Firebase modules here
-
-
-
+import Firebase
+import FirebaseFirestore
 
 class LocationRepository: ObservableObject {
   // Set up properties here
+  let path: String = "location_scans"
+  let store = Firestore.firestore()
   
-  
+  @Published var locations: [Location] = []
+  private var cancellables: Set<AnyCancellable> = []
   
   init() {
-    get()
+    self.get()
   }
 
   func get() {
     // Complete this function
+      store.collection(path)
+          .addSnapshotListener { querySnapshot, error in
+              if let error = error {
+                  print("error getting location: \(error.localizedDescription)")
+                  return
+              }
+              self.locations = querySnapshot?.documents.compactMap { document in
+                  try? document.data(as: Location.self)
+              } ?? []
+        }
     
-    
-    
-    
-    
-
   }
 }
--- PriceCheckFB/PriceCheckFBApp.swift
+++ PriceCheckFB/PriceCheckFBApp.swift
@@ -5,8 +5,7 @@
 
 import SwiftUI
 // import Firebase modules here
-
-
+import Firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
